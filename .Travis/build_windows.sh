de15ca02799a30103f4e1111664e112a146a2f47
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2019-09-30 19:24:15 +0200

    added doxygen

--- .Travis/build_windows.sh
+++ .Travis/build_windows.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
 git clone https://github.com/glfw/glfw.git include/glfw
+
+cd "dependencies/doxygen"
+mkdir "build"
+cd "build"
+cmake -G "Unix Makefiles" ..
+make
+make install
+cd "../.."
 
 cd "include/glfw"
 cmake .
 make -j2
-
 cd "../.."
+
 mkdir -p "build" 
 cd "build"
 
