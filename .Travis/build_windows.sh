c683ff402217851361419829e8c18811ac644276
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2019-09-30 22:03:00 +0200

    getting overview of files  in windows build

--- .Travis/build_windows.sh
+++ .Travis/build_windows.sh
@@ -5,10 +5,15 @@ mkdir -p "out"
 cd "out"
 cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_SHARED_LIBS=ON ..
 cmake --build .
-ls -a
+echo "--- contentes of win32 ---"
+ls -a "win32"
+echo "--- contentes of win32/Debug ---"
+ls -a "win32/Debug"
+echo "--- contentes of Debug ---"
+ls -a "Debug"
 cd "../../.."
 mkdir -p "lib"
-cp "dependencies/glfw/out/src/glfw3.dll" "lib"
+cp "dependencies/glfw/out/src/win32/Debug/glfw3.dll" "lib"
 
 mkdir -p "build" 
 cd "build"
