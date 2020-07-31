76ccb3da9e61015c0de76f7a25b087afde9910b6
noasakurajin <noasakurajin@web.de>
2020-07-31 14:50:55 +0200

    switch from master to main branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-find_compiler(){
-    # get cross-distro version of POSIX command
-    COMMAND=""
-    if command -v command 2>/dev/null; then
-        COMMAND="command -v"
-    elif type type 2>/dev/null; then
-        COMMAND="type"
-    fi
-
-    for comp in clang-12 clang-11 clang-10 clang-9 gcc-10 gcc-9 C:/Program Files/LLVM/bin/clang.exe; do
-        if $COMMAND "$comp" 2>/dev/null; then
-            export CC="$comp"
-            break;
-        fi
-    done
-
-    for comp in clang++-12 clang++-11 clang++-10 clang++-9 g++-10 g++-9 C:/Program Files/LLVM/bin/clang++.exe; do
-        if $COMMAND "$comp" 2>/dev/null; then
-            export CXX="$comp"
-            break;
-        fi
-    done
-}
-
 THREADS="3"
 LIBBUILDNAME="lib"
 SETUP="0"
@@ -157,7 +133,7 @@ else
     exit 1
 fi
 
-find_compiler
+source ./scripts/configure_compiler.sh
 
 echo "number of THREADS:$THREADS"
 echo "buildname:$LIBBUILDNAME"
