a6be42d42cfa729ef3bc7feb826663d6653db505
noasakurajin <noasakurajin@web.de>
2020-08-14 13:22:41 +0200

    rework of input_system #44

--- include/redhand/input.hpp
+++ include/redhand/input.hpp
@@ -16,6 +16,7 @@
 #include <memory>
 #include <functional>
 #include <string>
+#include <stdexcept>
 
 namespace redhand {
 
@@ -23,44 +24,120 @@ namespace redhand {
 
     /**
      * @brief The input_system is used to handle user inputs for all kinds of devices.
-     * @todo finish documentation of members
      * 
      */
     class input_system {
-
+        friend class engine;
       private:
+        /**
+         * @brief Construct a new input system object
+         * @warning this must not be called by anything except getInstatnce()
+         * 
+         */
         input_system(void) {
-        }
+        };
 
+        ///disable compy constructor
         input_system(input_system const &);
 
+        ///disable copy assignment
         void operator=(input_system const &);
 
-        engine *m_engine;
+        /**
+         * @brief a pointer to the attached engine
+         * 
+         */
+        engine* m_engine;
 
-      public:
+        /**
+         * @brief Get a reference to the attached engine, will throw an execption if m_engine is not set.
+         * 
+         * @return engine& A reference to the atteched engine.
+         */
+        engine& getEngineReference();
+
+        /**
+         * @brief This function sets m_engine
+         * 
+         * @param main_engine the engine that will be attached
+         */
+        void registerEngine(engine* main_engine);
+
+        /**
+         * @brief This function implements the getKeyAction function
+         * 
+         * @param wantedKey 
+         * @return key_actions 
+         */
+        key_actions impl_getKeyAction(keyboard_keys wantedKey);
+
+        /**
+         * @brief inplements the isKeyPressed function
+         * 
+         * @param wantedKey the key you want to know about
+         * @return true the key is pressed
+         * @return false the key is not pressed
+         */
+        bool impl_isKeyPressed(keyboard_keys wantedKey);
+
+        /**
+         * @brief This function returns an instance of the input_system
+         * 
+         * @return input_system& a reference to the input_system
+         */
         static input_system &getInstance() {
             static input_system instance;
             return instance;
         }
 
+      public:
+
+        /**
+         * @brief returns the state of a given key
+         * 
+         * @param wantedKey the key you want to know about
+         * @return key_actions the current state of that key
+         */
+        static key_actions getKeyAction(keyboard_keys wantedKey) {
+            return getInstance().impl_getKeyAction(wantedKey);
+        };
+
+        /**
+         * @brief check if a given key is pressed
+         * 
+         * @param wantedKey the key you want to know about
+         * @return true the key is pressed
+         * @return false the key is not pressed
+         */
+        static bool isKeyPressed(keyboard_keys wantedKey) {
+            return getInstance().impl_isKeyPressed(wantedKey);
+        };
+
+        /**
+         * @brief Get the active engine, this will throw an error if no engine is attached
+         * 
+         * @return engine& A reference to the active engine
+         */
+        static engine& getEngine(){
+            return getInstance().getEngineReference();
+        }
+        
+        /**
+         * @warning will be removed in 0.2.0
+         * @todo remove
+         */
         static key_actions static_getKeyAction(keyboard_keys wantedKey) {
-            return getInstance().getKeyAction(wantedKey);
+            return input_system::getKeyAction(wantedKey);
         };
 
+        /**
+         * @warning will be removed in 0.2.0
+         * @todo remove
+         */
         static bool static_isKeyPressed(keyboard_keys wantedKey) {
-            return getInstance().isKeyPressed(wantedKey);
+            return input_system::isKeyPressed(wantedKey);
         };
-
-        static engine *getEngine() {
-            return getInstance().m_engine;
-        };
-
-        void registerEngine(engine *main_engine);
-
-        key_actions getKeyAction(keyboard_keys wantedKey);
-
-        bool isKeyPressed(keyboard_keys wantedKey);
+        
     };
 
 } // namespace redhand
